0717bcd512c1a6c4b0c5cfd4a1f8a2b5259d8978
FizzWinPie <sulixhoni2000@gmail.com>
2023-12-29 14:33:01 -0500

    DoneTask1

--- client3.py
+++ client3.py
@@ -35,14 +35,16 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price)/2   #added ask_price and found the mean between the bid and ask
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    #updated the function
+    if (price_b) == 0:
+        # when price_b = 0 then avoid throwing ZeroDivisionError
+        return
+    return price_a/price_b  #returned this instead of 1
 
 
 # Main
@@ -52,8 +54,10 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        prices = {} #new dictionary
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price   #updated the dict
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"])) #updated the print func to add prices
